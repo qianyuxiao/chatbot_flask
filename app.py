55c9c57d04e21c5fbf942cff5554247c3b6d577f
qianyuxiao <qianyucazelles@ecg.camp>
2024-02-19 16:44:56 +0000

    update code with zephyr

--- app.py
+++ app.py
@@ -1,13 +1,62 @@
 from flask import Flask, render_template, request, jsonify
 
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer,TextIteratorStreamer
+from threading import Thread
 import torch
 
 
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
+# Set model and tokenizer
+def get_model_and_tokenizer(
+    llm_model_name = "HuggingFaceH4/zephyr-7b-beta",
+    torch_dtype = torch.bfloat16):
+    
+    # Set the CUDA device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("CUDA available:", torch.cuda.is_available())
+    torch.cuda.set_device(device)
+
+    # initiate model and tokenizer
+    model = AutoModelForCausalLM.from_pretrained(llm_model_name,
+                                                torch_dtype=torch_dtype, 
+                                                load_in_4bit=True)
+
+    tokenizer = AutoTokenizer.from_pretrained(llm_model_name,
+                                            torch_dtype=torch_dtype)
+    return model, tokenizer
+
+# define stream function
+def z_stream(user_prompt,model,tokenizer):
+
+    # system_prompt = 'You are ECG AI, an intelligent assistant dedicated to providing effective solutions. Your responses will include engaging touch. Analyze user queries and provide clear and practical answers. Focus on delivering solutions that are accurate, actionable, and helpful. If additional information is required for a more precise solution, politely ask clarifying questions. Your goal is to assist users by providing effective and reliable solutions to their queries.'
+    system_prompt = 'You are ECG AI, an intelligent assistant dedicated to providing effective solutions. Your responses will include engaging touch. Analyze user queries and provide clear and practical answers. Focus on delivering solutions that are accurate, actionable, and helpful. If you have no solution or additional information is required for a more precise solution, politely ask clarifying questions but make it short. Your goal is to assist users by providing effective and reliable solutions to their queries.'
+
+    E_INST = "</s>"
+    user, assistant = "<|user|>", "<|assistant|>"
+
+    prompt = f"{system_prompt}{E_INST}\n{user}\n{user_prompt.strip()}{E_INST}\n{assistant}\n"
+
+    inputs = tokenizer([prompt], return_tensors="pt").to('cuda')
+
+    streamer = TextIteratorStreamer(tokenizer,  skip_prompt=True, skip_special_tokens=True)
+
+    # Run the generation in a separate thread, so that we can fetch the generated text in a non-blocking way.
+    generation_kwargs = dict(inputs, streamer=streamer, max_new_tokens=1000)
+
+    thread = Thread(target=model.generate, kwargs=generation_kwargs)
+    thread.start()
+    # generated_text = ""
+    # for _, new_text in enumerate(streamer):
+    #     generated_text += new_text
+    # return generated_text
+    for _, new_text in enumerate(streamer):
+        yield new_text
+
+# load model and tokenizer
+model, tokenizer = get_model_and_tokenizer()
 
 app = Flask(__name__)
 
@@ -20,25 +69,15 @@ def index():
 def chat():
     msg = request.form["msg"]
     input = msg
-    return get_Chat_response(input)
+    print("question:",input)
+    response = get_Chat_response(input)
+    print("response:", response)
+    return response
 
 
 def get_Chat_response(text):
-
-    # Let's chat for 5 lines
-    for step in range(5):
-        # encode the new user input, add the eos_token and return a tensor in Pytorch
-        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
-
-        # append the new user input tokens to the chat history
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-
-        # generated a response while limiting the total chat history to 1000 tokens, 
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-
-        # pretty print last ouput tokens from bot
-        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    return z_stream(text,model,tokenizer)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='10.0.0.2', port=8502)
